80c91bf9112a6744c09529bfbe5283df80a3eca3
nikitadugar <nikita@clouddrove.com>
2020-03-25 21:09:00 +0530

    fix labels managedby variables

--- _example/complete-function/example.tf
+++ _example/complete-function/example.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "lambda" {
-  source = "git::https://github.com/clouddrove/terraform-aws-lambda.git?ref=tags/0.12.2"
+  source = "../../"
 
   name        = "lambda"
   application = "clouddrove"
