a57f2e6f31f4772afc9549e9fc74ae850540d3cb
nikitadugar <nikita@clouddrove.com>
2020-04-15 15:19:53 +0530

    change to fix a little bug

--- slack/slack/build_layer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PKG_DIR="python"
-rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR} && docker run -rm -v $(pwd):/foo lambci/lambda:build-python3.8
-pip install -r requirements.txt --no-deps -t ${PKG_DIR}
