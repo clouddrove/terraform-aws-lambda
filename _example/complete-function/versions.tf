b055182367ce590e6ed399420cc67580d2773b01
mamrajyadav <themamraj0131@gmail.com>
2023-10-24 20:58:11 +0530

    fix: update tf and aws latest version

--- _example/complete-function/versions.tf
+++ _example/complete-function/versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.6.1"
+  required_version = ">= 1.6.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.20.1"
+      version = ">= 5.22.0"
     }
   }
 }
